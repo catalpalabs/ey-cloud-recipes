f403760943064e66af8d0708697c9cd31b3ac0eb
Noel Jorden <njorden@gmail.com>
2011-12-14 11:39:49 -0700

    Updating recipe for larger staging server and solr 3.3

--- cookbooks/solr/recipes/default.rb
+++ cookbooks/solr/recipes/default.rb
@@ -3,17 +3,11 @@
 # Recipe:: default
 #
 # We specify what version we want below.
-solr_desiredversion = 1.4
+solr_desiredversion = 3.3
 if ['solo', 'util'].include?(node[:instance_role])
-  if solr_desiredversion == 1.3
-    solr_file = "apache-solr-1.3.0.tgz"
-    solr_dir = "apache-solr-1.3.0"
-    solr_url = "http://mirror.its.uidaho.edu/pub/apache/lucene/solr/1.3.0/apache-solr-1.3.0.tgz"
-  else
-    solr_dir = "apache-solr-1.4.1"
-    solr_file = "apache-solr-1.4.1.tgz"
-    solr_url = "http://mirror.candidhosting.com/pub/apache//lucene/solr/1.4.1/apache-solr-1.4.1.tgz"
-  end
+  solr_dir = "apache-solr-3.3.0"
+  solr_file = "apache-solr-3.3.0.tgz"
+  solr_url = "http://mirror.candidhosting.com/pub/apache//lucene/solr/3.3.0/apache-solr-3.3.0.tgz"
 
   directory "/var/run/solr" do
     action :create
